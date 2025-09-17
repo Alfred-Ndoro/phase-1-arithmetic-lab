70d8543ea030851cee59f42034fd4019fe42db08
Alfred-Ndoro <alfred.ndoro@outlook.com>
2025-09-17 19:34:08 +0300

    arithmetic assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()+1);
+
+let num3 = 22;
+let num4 = 6;
+
+let mod = num3 % num4;
+
+let max = Math.max(11,17,19,20,13);
+
+console.log("Multiply:", multiply);
+console.log("Random:", random)
+console.log("Mod:", mod);
+console.log("Max:", max)
